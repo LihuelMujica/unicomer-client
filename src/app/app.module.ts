d5578d72640e3b783a675f2d01ead27b25dc759e
lihuelmujica <lihuelmujica@gmail.com>
2023-08-06 17:51:13 -0300

    add: register form component and register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     LoginFormComponent,
-    HomePageComponent
+    HomePageComponent,
+    RegisterPageComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
